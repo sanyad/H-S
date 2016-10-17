5cd12dfd3f4e7c37e128bc39e19124df38e100a6
Alex_d <alexdwork9@gmail.com>
2016-10-17 17:33:49 +0300

    response

--- src/app/js.js
+++ src/app/js.js
@@ -4,10 +4,6 @@ angular.module('nordic').controller('CollapseDemoCtrl', function ($scope) {
   $scope.isCollapsedHorizontal = false;
 });
 angular.module('nordic').controller('CollapseDemoCtrl2', function ($scope) {
-  $scope.isNavCollapsed = true;
-  $scope.isCollapsed = false;
-  $scope.isCollapsed2 = false;
-  $scope.isCollapsed3 = false;
-  $scope.isCollapsed4 = false;
+  $scope.isCollapsed = true;
   $scope.isCollapsedHorizontal = false;
 });
